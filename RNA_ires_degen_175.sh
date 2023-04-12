1b1cfbd225735f6a43ec4526e458e3d53ea75706
Jamtai <87325533+Jamtai@users.noreply.github.com>
2023-04-12 11:59:13 -0500

    Update RNA_ires_degen_175.sh

--- RNA_ires_degen_175.sh
+++ RNA_ires_degen_175.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-#SBATCH -n 4                # Number of cores
-#SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 4-00:00          # Runtime in D-HH:MM
-#SBATCH --mem=40000         # Memory pool for all cores
-#SBATCH -w compute001
-#SBATCH -o group1.out     # File to which STDOUT will be written
-#SBATCH -e group1.er
-#SBATCH --mail-user=jjiamut@clemson.edu
-#
+
 #module
 
 ###Secretariat source /opt/ohpc/pub/Software/mamba-rocky/etc/profile.d/conda.s
