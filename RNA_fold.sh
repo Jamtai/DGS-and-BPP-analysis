86a65ed7fa18faf4927401e4e1e8304f18286f61
Jamtai <87325533+Jamtai@users.noreply.github.com>
2023-04-12 12:22:20 -0500

    Update RNA_fold.sh

--- RNA_fold.sh
+++ RNA_fold.sh
@@ -1,8 +1,19 @@
 module add rnastructure
 mkdir fold
+while read line
+do
+    if [[ ${line:0:1} == '>' ]]
+    then
+        outfile=${line#>}.fa
+        echo $line > "$outfile"
+    else
+        echo $line >> "$outfile"
+    fi
+done < output.fasta
 for F in *.fa
 do
     C=$(basename $F.ct) ;
     Fold-smp $F /fold/$C -mfe  ;
 done
+rm *.fa
 module unload rnastructure
