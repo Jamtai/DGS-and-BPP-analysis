c812129224b7c3ea981ec3d66d66982487f42a02
Jamtai <87325533+Jamtai@users.noreply.github.com>
2023-04-12 12:01:29 -0500

    Update RNA_add_partition.sh

--- RNA_add_partition.sh
+++ RNA_add_partition.sh
@@ -1,28 +1,7 @@
-#!/bin/bash
-#SBATCH -n 4                # Number of cores
-#SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 4-00:00          # Runtime in D-HH:MM
-#SBATCH --mem=40000         # Memory pool for all cores
-#SBATCH -w compute001
-#SBATCH -o group1.out     # File to which STDOUT will be written
-#SBATCH -e group1.er
-#SBATCH --mail-user=jjiamut@clemson.edu
-#
-#module
 module add rnastructure
 source /opt/ohpc/pub/Software/mamba-rocky/etc/profile.d/conda.sh
 export OMP_NUM_THREADS=10
 #split combined fasta into individual sequences
-while read line
-do
-    if [[ ${line:0:1} == '>' ]]
-    then
-        outfile=${line#>}.fa
-        echo $line > "$outfile"
-    else
-        echo $line >> "$outfile"
-    fi
-done < out.fasta
 #loop
 #in directory of fasta seqs
 for F in *.fa; do
